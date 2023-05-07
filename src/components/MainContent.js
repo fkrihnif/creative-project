6fd0ebd2d6b8aef9afee883c7436b72642c6bf6b
fikri hanif <fkrihnif@gmail.com>
2023-05-07 15:10:07 +0700

    learn

---
+++ src/components/MainContent.js
@@ -0,0 +1,13 @@
+import Hero from "./Hero";
+import Cards from "./Card";
+
+const MainContent = ()=> {
+    return (
+        <div>
+            <Hero />
+            <Cards />
+        </div>
+    )
+}
+
+export default MainContent;
